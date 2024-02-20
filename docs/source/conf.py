70a88aacb3ca1c492a7f304920696624f9db58ee
qizhanxing <qizhanxing@consen.net>
2024-02-20 14:07:17 +0800

    modify theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 #html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinx_sizzle_theme'
+html_theme = 'sizzle'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
